b228a42331b9c74e787cd7c613f4a06c703e3ac7
Prashanth Vishwakarma <82275050+v-prashanth@users.noreply.github.com>
2026-06-28 13:07:06 +0530

    fix: add newly generated premium tankless and heatpump images to showcase

--- components/home/FeaturedSolutions.tsx
+++ components/home/FeaturedSolutions.tsx
@@ -62,7 +62,7 @@ export const FeaturedSolutions: React.FC = () => {
             <Reveal delay={0.1} className="w-full">
               <div className="relative w-full h-[250px] sm:h-[370px] md:h-[430px]">
                 <Image
-                  src="/images/projects/project-wap-5.jpg"
+                  src="/images/tankless_showcase.png"
                   alt="Premium bathroom vanity layout concealing Stiebel Eltron tankless heating unit"
                   fill
                   className="object-cover object-center transition-transform duration-700 hover:scale-105"
@@ -83,7 +83,7 @@ export const FeaturedSolutions: React.FC = () => {
             <Reveal delay={0.1} className="w-full">
               <div className="relative w-full h-[250px] sm:h-[370px] md:h-[450px]">
                 <Image
-                  src="/images/projects/project-wap-18.jpg"
+                  src="/images/heatpump_showcase.png"
                   alt="Central thermal heat pump installation on a high-end villa terrace"
                   fill
                   className="object-cover object-center transition-transform duration-700 hover:scale-105"
